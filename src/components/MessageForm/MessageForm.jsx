a6b896b8ae01ed00c78ea997781f07deca65e9b8
ardalanmemar93 <ardalanmemar@gmail.com>
2024-01-26 18:19:00 -0800

    working on the form fields

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -6,11 +6,24 @@ const MessageForm = ({ templatesData, wordsData, conjunctionsData }) => {
     template: '',
     words: [],
     conjunctions: '',
+    additionalTemplate: '',  // New state for additional template
+    additionalWords: [],    // New state for additional words
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleAdditionalWordsChange = (e) => {
+    const { options } = e.target;
+    const selectedWords = [];
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        selectedWords.push(options[i].value);
+      }
+    }
+    setFormData((prevData) => ({ ...prevData, additionalWords: selectedWords }));
   };
 
   const handleSubmit = (e) => {
@@ -61,19 +74,55 @@ const MessageForm = ({ templatesData, wordsData, conjunctionsData }) => {
       <div>
         <label htmlFor="conjunctions">Conjunctions:</label>
         <select
-        id="conjunctions"
-        name="conjunctions"
-        value={formData.conjunctions}
-        onChange={handleChange}
+          id="conjunctions"
+          name="conjunctions"
+          value={formData.conjunctions}
+          onChange={handleChange}
         >
-        <option value="">Select Conjunction</option>
-        {conjunctionsData.map((conjunctionOption) => (
+          <option value="">Select Conjunction</option>
+          {conjunctionsData.map((conjunctionOption) => (
             <option key={conjunctionOption.name} value={conjunctionOption.name}>
-            {conjunctionOption.name}
+              {conjunctionOption.name}
             </option>
-        ))}
+          ))}
         </select>
-    </div>
+      </div>
+
+      {/* Additional Templates Input */}
+      <div>
+        <label htmlFor="additionalTemplate">Additional Template:</label>
+        <select
+          id="additionalTemplate"
+          name="additionalTemplate"
+          value={formData.additionalTemplate}
+          onChange={handleChange}
+        >
+          <option value="">Select Additional Template</option>
+          {templatesData.map((templateOption) => (
+            <option key={templateOption.name} value={templateOption.name}>
+              {templateOption.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Additional Words Input */}
+      <div>
+        <label htmlFor="additionalWords">Additional Words:</label>
+        <select
+          id="additionalWords"
+          name="additionalWords"
+          multiple
+          value={formData.additionalWords}
+          onChange={handleAdditionalWordsChange}
+        >
+          {wordsData.map((wordOption, index) => (
+            <option key={`${wordOption.name}-${index}`} value={wordOption.name}>
+              {wordOption.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
       {/* Submit Button */}
       <button type="submit">Create Message</button>
