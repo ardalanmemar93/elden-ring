5053bb040b4f99f2924b6c46470f47a28097f945
ardalanmemar93 <ardalanmemar@gmail.com>
2024-01-29 12:20:18 -0800

    adding some styling

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -5,8 +5,10 @@ const Message = ({ message }) => {
   const { template, words, conjunctions, additionalTemplate, additionalWords } = message;
 
   return (
-    <li>
-      {`${template.replace(/\*\*\*\*/g, words)} ${conjunctions} ${additionalTemplate.replace(/\*\*\*\*/g, additionalWords)}`}
+    <li className="bg-green-950 p-4 mb-2 rounded-md shadow-md transition duration-300 ease-in-out hover:bg-green-800">
+      <p className="text-lg font-bold text-white">
+        {`${template.replace(/\*\*\*\*/g, words)} ${conjunctions} ${additionalTemplate.replace(/\*\*\*\*/g, additionalWords)}`}
+      </p>
     </li>
   );
 };
